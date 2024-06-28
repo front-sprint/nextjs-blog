f5ba15df3e81638929b94283cf71c35f23a008c3
김태관 <iamtk@neowiz.com>
2024-06-28 10:05:44 +0900

    SSG, SSR, CSR 예제 추가

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,47 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "@/components/Layout";
+// import { getSortedPostsData } from "@/lib/posts";
+import { useEffect, useState } from "react";
 
-export default function Home() {
+// SSR (Server Side Rendering)
+// export async function getServerSideProps() {
+//   const allPostsData = getSortedPostsData();
+//   return {
+//     props: {
+//       allPostsData,
+//     },
+//   };
+// }
+
+// SSG (Static Site Generate)
+// export async function getStaticProps() {
+//   const allPostsData = getSortedPostsData();
+//   return {
+//     props: {
+//       allPostsData,
+//     },
+//   };
+// }
+
+// SSG (Static Site Generate) - Local API
+export async function getStaticProps() {
+  const response = await fetch("http://localhost:3000/api/posts");
+  const json = await response.json();
+
+  return {
+    props: {
+      allPostsData: json.allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
+  // const [allPostsData, setAllPostsData] = useState([]);
+  // useEffect(() => {
+  //   fetch("/api/posts")
+  //     .then((res) => res.json())
+  //     .then((data) => setAllPostsData(data.allPostsData));
+  // });
   return (
     <Layout home>
       <Head>
@@ -13,6 +53,20 @@ export default function Home() {
           (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+      </section>
+      <section className="text-[1.2rem] leading-normal pt-px">
+        <h2 className="text-2xl leading-[1.4] mx-0 my-4">Blog</h2>
+        <ul className="m-0 p-0">
+          {allPostsData.map(({ id, date, title }) => (
+            <li className="mt-0 mb-5 mx-0" key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
